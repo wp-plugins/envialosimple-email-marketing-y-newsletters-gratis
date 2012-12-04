05865c1d323de0044759c63847b85547fdca89ff
dattatec.com <dattatec.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 15:18:06 +0000

    Change Plugin Name
    git-svn-id: https://plugins.svn.wordpress.org/envialosimple-email-marketing-y-newsletters-gratis/trunk@634045 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- envialosimple-email-marketing-y-newsletters-gratis.php
+++ envialosimple-email-marketing-y-newsletters-gratis.php
@@ -1,6 +1,6 @@
 <?php
 /*
- Plugin Name: EnvialoSimple: Email Marketing y Newsletters GRATIS
+ Plugin Name: EnvialoSimple: Email Marketing y Newsletters
  Plugin URI: http://envialosimple.com/envialosimple-wordpress-plugin
  Description: El plugin de EnvialoSimple te permitirá crear y enviar Newsletters de calidad profesional, en minutos y directamente desde tu Wordpress.
  Version: 1.5
