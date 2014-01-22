1aa25fcc1cbd716437c815908c390bb977d13c39
dattatec.com <dattatec.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-22 11:38:06 +0000

    FIX XSS
    git-svn-id: https://plugins.svn.wordpress.org/envialosimple-email-marketing-y-newsletters-gratis/trunk@843064 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- paginas/vista-previa-form.php
+++ paginas/vista-previa-form.php
@@ -1,7 +1,11 @@
+<?php
+$administratorID = intval($_REQUEST['AdministratorID']);
+$formID = intval($_REQUEST['FormID']);
+?>
 <html>
     <head></head>
     <body>
         <div style="width:90%;height:90% ;position: absolute;z-index: 100"></div>
-        <script type='text/javascript' src='http://v2.envialosimple.com/form/show/AdministratorID/<?php echo $_REQUEST['AdministratorID'];?>/FormID/<?php echo $_REQUEST['FormID'];?>/format/widget'></script>
+        <script type='text/javascript' src='http://v2.envialosimple.com/form/show/AdministratorID/<?php echo $administratorID;?>/FormID/<?php echo $formID;?>/format/widget'></script>
     </body>
 </html>
